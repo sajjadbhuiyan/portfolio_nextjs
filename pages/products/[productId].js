f694737ef0e3dee42543faf51693bd1241a8685d
sajjadbhuiyan <sajjadbhuiyan2018@gmail.com>
2022-01-16 20:48:12 +0600

    pre-rendering error solve

--- pages/products/[productId].js
+++ pages/products/[productId].js
@@ -1,38 +1,43 @@
-import { useRouter } from "next/router";
-function Product({product}){
+import { useRouter } from 'next/router'
 
-    const router = useRouter()
-    if (router.isFallback) {
-        <div>Loading...</div>
-    }
+function Product({ product }) {
+  const router = useRouter()
 
-    return (
-        <>
-            <h1>Product Details</h1>
-            <p>
-                {product.description}
-            </p>
-        </>
-    )
+  if (router.isFallback) {
+    return <div>Loading...</div>
+  }
+  return (
+    <div>
+      <h2>
+        {product.id} {product.title} {product.price}
+      </h2>
+      <p>{product.description}</p>
+      <hr />
+    </div>
+  )
 }
 
-export default Product;
+export default Product
 
-export async function getStaticProps(context){
-    const {params} = context;
-    const response = await fetch(`http://localhost:4000/products/${params.productId}`)
-    const data = await response.json()
+export async function getStaticProps(context) {
+  const { params } = context
+  const response = await fetch(
+    `http://localhost:4000/products/${params.productId}`
+  )
+  const data = await response.json()
+  console.log(`Generating page for /products/${params.productId}`)
 
-    return {
-        props:{
-            product: data
-        }
-    }
+  return {
+    props: {
+      product: data
+    },
+    revalidate: 10
+  }
 }
 
-export async function getStaticPaths(){
-    return {
-        paths: [{ params: { productId: '1' } }],
-        fallback: true
-      }
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { productId: '1' } }],
+    fallback: true
+  }
 }
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,34 +1,32 @@
-import Link from "next/link"
-
-function ProductsList({products}){
+function ProductList({ products }) {
     return (
-        <>
-         <h1>All Product</h1>
-         {
-             products.map(product =>{
-                 return(
-                    <dev key={product.id}>
-                       <Link href={`products/${product.id}`} passHref>
-                        <h2>{product.title}</h2>
-                        <h1>{product.price}</h1>
-                       </Link>
-                    </dev>
-                 )
-             })
-         }
-        </>
+      <>
+        <h1>List of products</h1>
+        {products.map(product => {
+          return (
+            <div key={product.id}>
+              <h2>
+                {product.id} {product.title} {product.price}
+              </h2>
+              <hr />
+            </div>
+          )
+        })}
+      </>
     )
-}
-
-export default ProductsList
-
-export async function getStaticProps(){
-    const response = await fetch("http://localhost:4000/products")
+  }
+  
+  export default ProductList
+  
+  export async function getStaticProps() {
+    console.log('Generating / Regenerating ProductList')
+    const response = await fetch('http://localhost:4000/products')
     const data = await response.json()
-
-    return{
-        props:{
-            products:data
-        }
+  
+    return {
+      props: {
+        products: data
+      },
+      revalidate: 30
     }
-}
+  }
